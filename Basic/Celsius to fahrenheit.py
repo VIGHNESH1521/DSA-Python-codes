866e6f1a9c333134d54dc01607c24e8ede580dab
VIGHNESH SUDHAKAR <vighneshsudhakar@gmail.com>
2022-06-22 20:11:04 +0530

    Add files via upload

---
+++ Basic/Celsius to fahrenheit.py
@@ -0,0 +1,5 @@
+celsius = float(input("Enter the value in degree celsius : "))
+
+fahrenheit = (celsius * 1.8) + 32
+
+print("The Value in fahrenheit is : {}".format(fahrenheit))
---
+++ Basic/Fahrenheit to celsius.py
@@ -0,0 +1,5 @@
+fahrenheit = float(input("Enter the value in fahrenheit : "))
+
+celsius = (fahrenheit - 32)/ 1.8
+
+print("The Value in fahrenheit is : {}".format(celsius))
