a9b7d80acbb7b1e45e3a7540b113c8c38df0fe1c
VIGHNESH SUDHAKAR <vighneshsudhakar@gmail.com>
2022-06-22 19:41:54 +0530

    Add files via upload

---
+++ Basic/KM_to_MILES.py
@@ -0,0 +1,4 @@
+km = float(input("Enter the kilometer : "))
+conversion_ratio = 0.621371
+miles = km * conversion_ratio
+print("The equivalent value of the kilometer to miles is : {} miles".format(miles))
